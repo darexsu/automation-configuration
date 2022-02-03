4dc22d329b2986158e151541d1e0cb9c3751b7fb
Darexsu <95475095+darexsu@users.noreply.github.com>
2022-02-03 15:07:16 +0500

    Update requirements.sh

--- ansible/requirements.sh
+++ ansible/requirements.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-if [ $1 != 'php' ]; then
+if [ "$1" != "php" ]; then
 ansible-galaxy install darexsu.php
+fi
+if [ "$1" != "mariadb" ]; then
+ansible-galaxy install darexsu.mariadb
 fi
