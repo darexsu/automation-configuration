d87143f7c006f12a908dff1286125386a85f76e0
Darexsu <95475095+darexsu@users.noreply.github.com>
2022-02-21 13:18:40 +0500

    Update requirements.sh

--- ansible/requirements.sh
+++ ansible/requirements.sh
@@ -9,3 +9,9 @@ fi
 if [ "$1" != "nginx" ]; then
 ansible-galaxy install darexsu.nginx
 fi
+if [ "$1" != "zabbix_gui" ]; then
+ansible-galaxy install darexsu.nginx
+fi
+if [ "$1" != "zabbix_server" ]; then
+ansible-galaxy install darexsu.nginx
+fi
