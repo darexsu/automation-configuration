b5a303c10718e7abdc9129b1db8c68a3fcaa75c3
Darexsu <95475095+darexsu@users.noreply.github.com>
2022-03-04 20:32:51 +0500

    Update requirements.sh

--- ansible/requirements.sh
+++ ansible/requirements.sh
@@ -3,23 +3,26 @@
 if [ "$1" != "php" ]; then
 ansible-galaxy install darexsu.php
 fi
-sleep 5s
+sleep 2s
 if [ "$1" != "mariadb" ]; then
 ansible-galaxy install darexsu.mariadb
 fi
-sleep 5s
+sleep 2s
 if [ "$1" != "nginx" ]; then
 ansible-galaxy install darexsu.nginx
 fi
-sleep 5s
+sleep 2s
 if [ "$1" != "zabbix_gui" ]; then
 ansible-galaxy install darexsu.zabbix_gui
 fi
-sleep 5s
+sleep 2s
 if [ "$1" != "zabbix_server" ]; then
 ansible-galaxy install darexsu.zabbix_server
 fi
-sleep 5s
+sleep 2s
 if [ "$1" != "apache" ]; then
 ansible-galaxy install darexsu.apache
+fi
+if [ "$1" != "mysql" ]; then
+ansible-galaxy install darexsu.mysql
 fi
