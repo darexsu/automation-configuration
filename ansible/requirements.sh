4efaeb09afa08a46d4d0a2fd1749fe99c2008949
Darexsu <95475095+darexsu@users.noreply.github.com>
2022-04-23 13:13:44 +0500

    Update requirements.sh

--- ansible/requirements.sh
+++ ansible/requirements.sh
@@ -4,10 +4,6 @@ if [ "$1" != "php" ]; then
 /usr/local/bin/ansible-galaxy install darexsu.php
 fi
 sleep 2s
-if [ "$1" != "mariadb" ]; then
-/usr/local/bin/ansible-galaxy install darexsu.mariadb
-fi
-sleep 2s
 if [ "$1" != "nginx" ]; then
 /usr/local/bin/ansible-galaxy install darexsu.nginx
 fi
@@ -28,10 +24,6 @@ if [ "$1" != "mysql" ]; then
 /usr/local/bin/ansible-galaxy install darexsu.mysql
 fi
 sleep 2s
-if [ "$1" != "firewalld" ]; then
-/usr/local/bin/ansible-galaxy install darexsu.firewalld
-fi
-sleep 2s
 if [ "$1" != "elasticsearch" ]; then
 /usr/local/bin/ansible-galaxy install darexsu.elasticsearch
 fi
@@ -40,18 +32,6 @@ if [ "$1" != "kibana" ]; then
 /usr/local/bin/ansible-galaxy install darexsu.kibana
 fi
 sleep 2s
-if [ "$1" != "logstash" ]; then
-/usr/local/bin/ansible-galaxy install darexsu.logstash
-fi
-sleep 2s
-if [ "$1" != "mongodb" ]; then
-/usr/local/bin/ansible-galaxy install darexsu.mongodb
-fi
-sleep 2s
 if [ "$1" != "openvpn" ]; then
 /usr/local/bin/ansible-galaxy install darexsu.openvpn
-fi
-sleep 2s
-if [ "$1" != "graylog" ]; then
-/usr/local/bin/ansible-galaxy install darexsu.graylog
 fi
