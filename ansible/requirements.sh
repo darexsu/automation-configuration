83a2096f31421beed3f674953e54879ae012f722
Darexsu <95475095+darexsu@users.noreply.github.com>
2022-05-18 20:20:22 +0500

    Update requirements.sh

--- ansible/requirements.sh
+++ ansible/requirements.sh
@@ -4,18 +4,14 @@ if [ "$1" != "php" ]; then
 /usr/local/bin/ansible-galaxy install darexsu.php
 fi
 sleep 2s
-if [ "$1" != "nginx" ]; then
-/usr/local/bin/ansible-galaxy install darexsu.nginx
-fi
-sleep 2s
-if [ "$1" != "mysql" ]; then
-/usr/local/bin/ansible-galaxy install darexsu.mysql
-fi
-sleep 2s
 if [ "$1" != "openvpn" ]; then
 /usr/local/bin/ansible-galaxy install darexsu.openvpn
 fi
 sleep 2s
 if [ "$1" != "gitlab" ]; then
 /usr/local/bin/ansible-galaxy install darexsu.gitlab
+fi
+sleep 2s
+if [ "$1" != "docker" ]; then
+/usr/local/bin/ansible-galaxy install darexsu.docker
 fi
