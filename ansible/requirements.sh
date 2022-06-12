fd1475c8876f470881064f640493e134f7f3f389
Darexsu <95475095+darexsu@users.noreply.github.com>
2022-06-12 23:11:25 +0500

    Update requirements.sh

--- ansible/requirements.sh
+++ ansible/requirements.sh
@@ -3,8 +3,5 @@
 if [ "$1" != "docker" ]; then
 /usr/local/bin/ansible-galaxy install darexsu.docker
 fi
-sleep 2s
-if [ "$1" != "lemp" ]; then
-/usr/local/bin/ansible-galaxy install darexsu.lemp
-fi
+
 
